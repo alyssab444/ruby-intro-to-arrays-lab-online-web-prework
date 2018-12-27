78c5ffc403645afb58f3b671857cb3f19e8f4d54
Alyssa Brown <alyssa.brown@live.com>
2018-12-27 02:09:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -25,7 +25,7 @@ end
 
 def first_element_with_array_methods
   south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  south_east_asia.first
+  return south_east_asia.first
 end
   
   
