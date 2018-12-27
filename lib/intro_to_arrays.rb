6d1b8a5c52f0f191fb829207ee45af51090039fd
Alyssa Brown <alyssa.brown@live.com>
2018-12-27 02:05:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -18,5 +18,10 @@ def third_element(array)
   taylor_swift[2]
 end
 
+def last_element(array)
+  taylor_swift= ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  taylor_swift[-1]
+end
+
   
   
