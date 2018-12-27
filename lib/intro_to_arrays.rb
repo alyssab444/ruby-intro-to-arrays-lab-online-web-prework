3aca6b8d71f4fc48190422c57c1bbd5657cded4e
Alyssa Brown <alyssa.brown@live.com>
2018-12-27 01:52:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,6 +8,8 @@ def array_with_two_elements
   return two_elements
 end 
 
-def first_element
-  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+def first_element(array)
+  taylor_swift= ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  
+  
   
